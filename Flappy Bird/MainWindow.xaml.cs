a4cfaa6ce95da2e1437741997737303d085b2ff3
Petrik Diák <diak@petrik.hu>
2026-01-07 09:19:38 +0100

    elso commit, ugral a madar

--- Flappy Bird/MainWindow.xaml.cs
+++ Flappy Bird/MainWindow.xaml.cs
@@ -1,24 +1,70 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace Flappy_Bird
+namespace FlappyBirdWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainWindow : Window
+	{
+		DispatcherTimer gameTimer = new DispatcherTimer();
+
+		double birdY = 200;
+		double birdVelocity = 0;
+		const double gravity = 0.6;
+		double jumpStrength = -10;
+
+		bool isGameOver = false;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			Focus();
+
+			Canvas.SetLeft(Bird, 100);
+			Canvas.SetTop(Bird, birdY);
+
+			gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+			gameTimer.Tick += GameLoop;
+			gameTimer.Start();
+		}
+
+		private void GameLoop(object sender, EventArgs e)
+		{
+			if (isGameOver) return;
+
+			birdVelocity += gravity;
+			birdY += birdVelocity;
+			Canvas.SetTop(Bird, birdY);
+
+			CheckBoundaries();
+		}
+
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Space && !isGameOver)
+				birdVelocity = jumpStrength;
+		}
+
+		void CheckBoundaries()
+		{
+			if (birdY < 0 || birdY > GameCanvas.ActualHeight - Bird.Height)
+				GameOver();
+		}
+
+
+		void GameOver()
+		{
+			isGameOver = true;
+			gameTimer.Stop();
+
+			GameOverText.Visibility = Visibility.Visible;
+			Canvas.SetLeft(GameOverText, 250);
+			Canvas.SetTop(GameOverText, 150);
+		}
+	}
 }
